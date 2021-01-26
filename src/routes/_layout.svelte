5b784f8653c831ec7a53cd0751186c810171770c
Dhimas Kirana <dhimaskirana@live.com>
2021-01-26 14:28:34 +0700

    hapus link style layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,3 @@
-<style>
-	@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
-</style>
 <script>
 	// import Nav from '../components/Nav.svelte';
 	import Footer from "../components/Footer.svelte";
